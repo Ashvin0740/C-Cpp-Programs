5de6f2d842ab883a625a48fc8b0aa7387e1b5a9e
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-01-17 16:46:00 +0530

    sum of two  number
    sum of two  number

---
+++ Sum2Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+
+    printf("Enter number 1:");
+    scanf("%d",&a);
+        printf("Enter number 2:");
+    scanf("%d",&b);
+    c = a+b;
+            printf("sum of 2 number is = %d",c);
+            getch();    
+
+}
