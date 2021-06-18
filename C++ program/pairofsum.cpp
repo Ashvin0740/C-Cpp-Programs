e604bef6d8f7b09fb3e7326eada70237dd41899c
Ashvin Vanol <67723298+Ashvin0740@users.noreply.github.com>
2021-06-18 09:48:27 +0530

    Update pairofsum.cpp

--- C++ program/pairofsum.cpp
+++ C++ program/pairofsum.cpp
@@ -1,28 +1,31 @@
 #include<iostream>
 using namespace std;
-
-
-int main(){
-
-    int a[] = {1,2,5,4,8,9,12,15,18};
-    int s = 5;
-    int i =0;
-    int  j = sizeof(a)/sizeof(int )-1;
-
-
-    while(i<j){
-        int current_sum = a[i] + a[j];
-    if (current_sum > s){
-        j--;
-    }else if(current_sum < s){
-        i++;
-    }else if(current_sum == s){
-        cout << a[i] << "and" << a[j] << endl;
-        i++;
-        j--;
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
     }
-}
+    sort(a,a+n);
+    int t;
+    cin>>t;
+    int l=0;
+    int r=n-1;
+    while(l<r){
+        if(a[l]+a[r]==t){
+            cout<<a[l]<<" and "<<a[r]<<endl;
+            l++;
+            r--;
+        }else if(a[l]+a[r]<t){
+            l++;
+        }else{
+            r--;
+        }
+        
+    }
     return 0;
-
 }
+
+
     
