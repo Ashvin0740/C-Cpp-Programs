fb0c7798a30af773ddda3ed65ca6b0df93938c79
Your Name <ashvinvanol44@gmail.com>
2021-06-25 12:16:56 +0530

    FindPeakElement

---
+++ C++ program/FindPeakElement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        int lo=0;
+        int hi=nums.size()-1;
+        while(lo<=hi){
+             int mid=(lo+hi)/2;
+            if(lo==hi){
+                return lo;
+            }
+            else if(nums[mid]<nums[mid+1]){
+                lo=mid+1;
+            }
+            else{
+                hi=mid;
+            }
+        }
+        return -1;
+        }
+};
