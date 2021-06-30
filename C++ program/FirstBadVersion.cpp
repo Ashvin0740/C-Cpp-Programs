aa7096f7f8495d3f0833e5b04c55e3762265b946
Your Name <ashvinvanol44@gmail.com>
2021-06-30 19:43:59 +0530

    FirstBadVersion

---
+++ C++ program/FirstBadVersion.cpp
@@ -0,0 +1,24 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo =0;
+        int hi = n;
+        int result =n;
+        
+        while(lo <=hi) {
+            int mid  = lo + (hi-lo)/2;
+            
+            if(isBadVersion(mid)){
+                result  = mid;
+                hi = mid -1;
+            }else{
+                lo = mid +1;
+            }
+        }
+        return result;
+        
+    }
+};
