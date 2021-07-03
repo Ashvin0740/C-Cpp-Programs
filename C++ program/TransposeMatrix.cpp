9d94b5f8341712ddd37e73c20dccb12f2b87a209
Your Name <ashvinvanol44@gmail.com>
2021-07-03 20:38:02 +0530

    TransposeMatrix

---
+++ C++ program/TransposeMatrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& a) {
+        int n = a.size(), m = a[0].size();
+        vector<vector<int>> res(m, vector<int>(n,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                res[j][i] = a[i][j];
+            }
+        }
+        return res;
+    }
+};
