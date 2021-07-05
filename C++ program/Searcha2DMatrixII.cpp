5fc3d796ff420e09d9257f2dd4daef6fc16460fa
Your Name <ashvinvanol44@gmail.com>
2021-07-05 20:22:25 +0530

    Searcha2DMatrixII

---
+++ C++ program/Searcha2DMatrixII.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int lo = 0;
+        int hi = m-1;
+        
+        while( lo<n && hi>=0){
+         if(matrix[lo][hi] == target){
+             return true;
+         }else if(matrix[lo][hi] < target){
+             lo++;
+         }else{
+             hi--;
+         }
+        }
+        return false;
+    }
+};
