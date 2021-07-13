b225172eb73cfa7fd7aca06a31ef640bbcf560c0
Your Name <67723298+Ashvin0740@users.noreply.github.com>
2021-07-13 18:32:34 +0530

    GrayCode

---
+++ C++ program/GrayCode.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+   
+    
+       if(n == 1) {
+            return {0, 1};
+       }
+        vector<int>prev = grayCode(n - 1);
+        
+        for(int i = prev.size() - 1; i >= 0; i--)
+            prev.push_back(prev[i] + pow(2, n - 1));
+        
+        return prev;  
+    }
+};
