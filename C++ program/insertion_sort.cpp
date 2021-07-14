5abb08b262a21870cc7f00c7b9a236134a4125eb
Your Name <67723298+Ashvin0740@users.noreply.github.com>
2021-07-14 18:56:10 +0530

    example
    example

--- C++ program/insertion_sort.cpp
+++ C++ program/insertion_sort.cpp
@@ -30,7 +30,7 @@ int main() {
 
         for(int i=0;i<n; i++){
             //  cout << "sorted list is";
-            cout << a[i] <<" ,";
+            cout << a[i] <<" ,";   
         }
        return 0;
 
---
+++ C++ program/recursion/AllocationGoogleKickstart.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5+ 5;
+    int n, b;
+    int a[N];
+    int main () {
+        int t;
+        cin >> t;
+        int tc =0;
+        while(t--){
+            tc++;
+            cin >> n>> b;
+            for(int i =1; i<=n;i++)
+            cin >> a[i];
+            sort(a+1,a+n+1);
+            int ans =0;
+            for(int i =1; i<=n; i++){
+                if(b>= a[i]){
+                    ans++;
+                    b-= a[i];
+                }
+
+            }
+            cout << "case #" << tc <<":" <<ans<< endl;
+
+        }
+    return 0;
+}
