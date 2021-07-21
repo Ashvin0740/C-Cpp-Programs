3391a62a6f5afd9fa77c8cfea2bab580f75835ed
Your Name <67723298+Ashvin0740@users.noreply.github.com>
2021-07-21 18:38:47 +0530

    MergeInBetweenLinkedLists

---
+++ C++ program/LinkedList/MergeInBetweenLinkedLists.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode*p,*q,*r;
+        p = list1;
+        q=list1;
+        r=list2;
+        while(r->next!=NULL){
+            r = r->next;
+        }
+        a= a-1;
+        while(a>0)
+        {
+            p = p->next;
+            a--;
+        }
+        b=b+1;
+        while(b>0){
+            q = q->next;
+            b--;
+        }
+        p->next = list2;
+        r->next = q;
+        return list1;
+    }
+};
