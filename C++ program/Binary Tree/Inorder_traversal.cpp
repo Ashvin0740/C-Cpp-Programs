9950a1a5342d01ef4c375f7e066cc41233d83faa
Your Name <67723298+Ashvin0740@users.noreply.github.com>
2021-08-17 20:00:38 +0530

    Inorder_traversal

---
+++ C++ program/Binary Tree/Inorder_traversal.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class node{
+public:
+	int data;
+	node*left;
+	node*right;
+
+	node(int d){
+		data = d;
+		left =NULL;
+		right = NULL;
+	}
+};
+node * buildTree() {
+	int d;
+	cin>>d;
+	if(d == -1){
+		return NULL;
+	}
+	node * root = new node(d);
+	root->left = buildTree();
+	root->right = buildTree();
+	return root;
+}
+void printIn(node * root){
+	if(root == NULL){
+		return;
+	}
+	// left root right
+	printIn(root->left);
+	cout << root->data<<" ";
+	printIn(root->right);
+
+}
+
+int main() {
+	node* root = buildTree();
+
+
+	printIn(root);
+
+	return 0;
+}
