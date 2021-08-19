8fcf3d0fd187d396e1f8f4f727790e1d74ce30fd
Your Name <67723298+Ashvin0740@users.noreply.github.com>
2021-08-19 16:57:14 +0530

    Binary_Tree_Level_Order_Traversal

---
+++ C++ program/Binary Tree/Binary_Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root) return {};
+        vector<vector<int>> result;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int count = q.size();
+            vector<int> level_nodes;
+            for(int i =0; i<count; i++){
+                TreeNode *node = q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                level_nodes.push_back(node->val);
+            }
+        result.push_back(level_nodes);
+
+        }
+      return result;
+
+    }
+};
